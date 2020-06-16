7c2d892298374aba8356ebde4fc417f70470d97b
anastasapro <anastasja.pro@gmail.com>
2020-06-16 16:25:22 +0300

    Update task.js
    ДЗ к лекции 1.3 «Типы данных». прохорова

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
-}
+  let percentNumber = parseFloat(percent);
+  let contributionNumber = parseFloat(contribution);
+  let amountNumber = parseFloat(amount);
+  if (isNaN(percentNumber)) {
+	  return `процентная ставка сожержит неверное значение "${percent}"`;
+  } else if (isNaN(contributionNumber)) {
+	  return `сумма первоначального взноса сожержит неверное значение "${contribution}"`;
+  } else if  (isNaN(amountNumber)){ 
+	  return `сумма кредита сожержит неверное значение "${amount}"`;
+  }
+      let P = percentNumber / 100 / 12;
+	  let n = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth();
+	  let totalAmount = ((amountNumber - contributionNumber) * (P + P / (((1 + P) ** n) - 1))) * n;
+	  console.log(totalAmount);
+	  return totalAmount.toFixed(2);
+  }
+	  
+  
+
+
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+	if (name === ""){
+	return ("Привет, мир! Меня зовут Аноним");
+	}else { 
+		return ("Привет, мир! Меня зовут " + name);
+	 }
+}	
+   
