ef44e8a08fe1bfd724d4aa890e88b48586d6c960
anastasapro <anastasja.pro@gmail.com>
2020-06-16 16:24:38 +0300

    Update task.js
    ДЗ к занятию 1.2 «Расширенный синтаксис». Прохорова

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
+"use strict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+	let d = Math.pow(b,2) - 4 * a * c;
+	if (d === 0){
+		x[0] = (-b / (2 * a));
+	}
+	else if (d > 0){
+		x[0] = ((-b - Math.sqrt(d)) /( 2 * a));
+		x[1] = ((-b + Math.sqrt(d)) / (2 * a));
+	}
+	else{
+	    x = [];	
+	}
+	return x;
 }
+
+// вторая задача
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+	if (marks.length > 5) {
+		marks.splice(5);
+	} else if (marks.length == 0) {
+	return 0;
+	} 
+	let sum = 0;
+	let averageMark = 0;
+	for (let i = 0; i < marks.length; i++){
+    sum += marks[i];
+	}
+	averageMark = sum/marks.length;
+	return averageMark;	
 }
 
+//третья задача
+
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+		if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) < 18) {
+		return ("Сожалею, " + "," + name + " но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!" );
+	 } else { 
+		return ("Не желаете ли олд-фэшн, " + name);
+	 }
 }
+	
+   
+    
