c413e2c4016fda68c93743fe62949b21221c0d21
anastasapro <anastasja.pro@gmail.com>
2020-06-25 17:36:58 +0300

    Update task.js
    ДЗ к лекции 2.2 «Прототип и конструктор объекта»

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,32 @@
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+  let str = this.replace(/[\s!-]/g,'').toLowerCase();
+  let arr = str.split('');
+  let arrreversed = str.split('');
+  arrreversed.reverse();
+  return (arr.join('') == arrreversed.join('')); 
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  if (marks.length === 0) {
+  return 0;
+  } 
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];		
+    }
+  let average = sum / marks.length;
+  let roundedAverage = Math.round(average);
+  return roundedAverage;
 }
 
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Date.now();
+  birthday = Date.parse(birthday);
+  let diff = now - birthday;
+  let age = diff / (86400000 * 365.25);
+  return age > 18;
 }
+
